9a770e6700107b3771d66a16c2d46800e145dae9
Matt Way <matt@laigames.com>
2015-08-26 17:03:54 +0800

    Use a gradient to colour generated image as grey scale doesn't have enough colours

--- SingaporeSkiing/Program.cs
+++ SingaporeSkiing/Program.cs
@@ -2,6 +2,7 @@
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
+using System.Linq;
 using System.Runtime.InteropServices;
 
 namespace SingaporeSkiing
@@ -187,11 +188,12 @@ namespace SingaporeSkiing
 				long altitudeIdx = i/3;
 
 				double normalisedAltitude = Math.Min(Math.Max(mapData.Altitudes[altitudeIdx]/(double) highestAltitude, 0f), 1f);
-				byte pixelValue = (byte) (normalisedAltitude*0xFF);
 
-				pixelBuffer[i + 0] = pixelValue;
-				pixelBuffer[i + 1] = pixelValue;
-				pixelBuffer[i + 2] = pixelValue;
+				var pixel = CalculateColour(normalisedAltitude);
+
+				pixelBuffer[i + 0] = pixel.B;
+				pixelBuffer[i + 1] = pixel.G;
+				pixelBuffer[i + 2] = pixel.R;
 			}
 
 			GCHandle pixelBufferHandle = GCHandle.Alloc(pixelBuffer, GCHandleType.Pinned);
@@ -205,6 +207,34 @@ namespace SingaporeSkiing
 			}
 
 			pixelBufferHandle.Free();
+		}
+
+		static Color CalculateColour (double offset)
+		{
+			var colours = new[] {
+				new Tuple<double, Color>(0, Color.FromArgb(255, 0, 0, 255)),
+				new Tuple<double, Color>(0.5f, Color.FromArgb(255, 0, 255, 0)),
+				new Tuple<double, Color>(1, Color.FromArgb(255, 255, 0, 0)),
+			};
+
+			Tuple<double, Color> before = colours.OrderBy(c => c.Item1).First();
+			Tuple<double, Color> after = colours.OrderByDescending(c => c.Item1).First();
+
+			foreach (var gradientStop in colours) {
+				if (gradientStop.Item1 < offset && gradientStop.Item1 > before.Item1) {
+					before = gradientStop;
+				}
+
+				if (gradientStop.Item1 > offset && gradientStop.Item1 < after.Item1) {
+					after = gradientStop;
+				}
+			}
+
+			return Color.FromArgb(
+				(byte) (((offset - before.Item1)*(after.Item2.A - before.Item2.A)/(after.Item1 - before.Item1) + before.Item2.A)),
+				(byte) (((offset - before.Item1)*(after.Item2.R - before.Item2.R)/(after.Item1 - before.Item1) + before.Item2.R)),
+				(byte) (((offset - before.Item1)*(after.Item2.G - before.Item2.G)/(after.Item1 - before.Item1) + before.Item2.G)),
+				(byte) (((offset - before.Item1)*(after.Item2.B - before.Item2.B)/(after.Item1 - before.Item1) + before.Item2.B)));
 		}
 	}
 }
